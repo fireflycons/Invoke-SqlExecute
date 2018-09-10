0a274fc804adfb8a3212e836244bb8c976f61123
fireflycons <a.m@ckay.me>
2018-09-10 21:53:48 +0100

    Add in pester framework

---
+++ TestAndDeploy.ps1
@@ -0,0 +1,29 @@
+param ($Task = 'Default')
+
+$currentLocation = Get-Location
+try
+{
+    Set-Location $PSScriptRoot
+
+    # Grab nuget bits, install modules, set build variables, start build.
+    Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
+
+    Install-Module Psake, PSDeploy, BuildHelpers -force -AllowClobber -Scope CurrentUser
+    Install-Module Pester -MinimumVersion 4.1 -Force -AllowClobber -SkipPublisherCheck -Scope CurrentUser
+    Import-Module Psake, BuildHelpers
+    Set-BuildEnvironment -ErrorAction SilentlyContinue
+
+    Invoke-psake -buildFile $ENV:BHProjectPath\psake.ps1 -taskList $Task -nologo
+    exit ( [int]( -not $psake.build_success ) )
+}
+catch
+{
+    Write-Error $_.Exception.Message
+
+    # Make AppVeyor fail the build if this setup borks
+    exit 1
+}
+finally
+{
+    Set-Location $currentLocation
+}
---
+++ psake.ps1
@@ -0,0 +1,168 @@
+# PSake makes variables declared here available in other scriptblocks
+# Init some things
+Properties {
+    # Find the build folder based on build system
+    $ProjectRoot = $ENV:BHProjectPath
+    if (-not $ProjectRoot)
+    {
+        $ProjectRoot = $PSScriptRoot
+    }
+    $ProjectRoot = Convert-Path $ProjectRoot
+
+    try
+    {
+        $script:IsWindows = (-not (Get-Variable -Name IsWindows -ErrorAction Ignore)) -or $IsWindows
+        $script:IsLinux = (Get-Variable -Name IsLinux -ErrorAction Ignore) -and $IsLinux
+        $script:IsMacOS = (Get-Variable -Name IsMacOS -ErrorAction Ignore) -and $IsMacOS
+        $script:IsCoreCLR = $PSVersionTable.ContainsKey('PSEdition') -and $PSVersionTable.PSEdition -eq 'Core'
+    }
+    catch { }
+
+    $Timestamp = Get-date -uformat "%Y%m%d-%H%M%S"
+    $PSVersion = $PSVersionTable.PSVersion.Major
+    $TestFile = "TestResults_PS$PSVersion`_$TimeStamp.xml"
+    $lines = '----------------------------------------------------------------------'
+
+    $Verbose = @{}
+    if ($ENV:BHCommitMessage -match "!verbose")
+    {
+        $Verbose = @{Verbose = $True}
+    }
+
+    $DefaultLocale = 'en-US'
+    $DocsRootDir = "$PSScriptRoot\docs"
+    $ModuleName = "PSCloudFormation"
+    $ModuleOutDir = "$PSScriptRoot\PSCloudFormation"
+
+}
+
+Task Default -Depends Deploy
+
+Task Init {
+    $lines
+    Set-Location $ProjectRoot
+    "Build System Details:"
+    Get-Item ENV:BH*
+    "`n"
+
+    if ($script:IsWindows)
+    {
+        "Checking for NuGet"
+        $psgDir = Join-Path ${env:LOCALAPPDATA} "Microsoft\Windows\PowerShell\PowerShellGet"
+
+        $nugetPath = $(
+
+            $nuget = Get-Command nuget.exe -ErrorAction SilentlyContinue
+
+            if ($nuget)
+            {
+                $nuget.Path
+            }
+            else
+            {
+                if (Test-Path -Path (Join-Path $psgDir 'nuget.exe'))
+                {
+                    Join-Path $psgDir 'nuget.exe'
+                }
+            }
+        )
+
+        if ($nugetPath)
+        {
+            "NuGet.exe found at '$nugetPath"
+        }
+        else
+        {
+            if (-not (Test-Path -Path $psgDir -PathType Container))
+            {
+                New-Item -Path $psgDir -ItemType Directory | Out-Null
+            }
+
+            "Installing NuGet to '$psgDir'"
+            Invoke-WebRequest -Uri https://nuget.org/nuget.exe -OutFile (Join-Path $psgDir 'nuget.exe')
+        }
+    }
+}
+
+Task Test -Depends Init {
+    $lines
+    "`n`tSTATUS: Testing with PowerShell $PSVersion"
+
+    # Gather test results. Store them in a variable and file
+    $pesterParameters = @{
+        Path         = "$ProjectRoot\Pester"
+        PassThru     = $true
+        OutputFormat = "NUnitXml"
+        OutputFile   = "$ProjectRoot\$TestFile"
+    }
+
+    if (-Not $IsWindows) { $pesterParameters["ExcludeTag"] = "WindowsOnly" }
+    $TestResults = Invoke-Pester @pesterParameters
+
+    # In Appveyor?  Upload our tests! #Abstract this into a function?
+    If ($ENV:BHBuildSystem -eq 'AppVeyor')
+    {
+        (New-Object 'System.Net.WebClient').UploadFile(
+            "https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)",
+            "$ProjectRoot\$TestFile" )
+    }
+
+    Remove-Item "$ProjectRoot\$TestFile" -Force -ErrorAction SilentlyContinue
+
+    # Failed tests?
+    # Need to tell psake or it will proceed to the deployment. Danger!
+    if ($TestResults.FailedCount -gt 0)
+    {
+        Write-Error "Failed '$($TestResults.FailedCount)' tests, build failed"
+    }
+    "`n"
+}
+
+Task Build -Depends Test {
+    $lines
+
+    # Load the module, read the exported functions, update the psd1 FunctionsToExport
+    #Set-ModuleFunctions
+
+    # Bump the module version if we didn't already
+    Try
+    {
+        [version]$GalleryVersion = Get-NextNugetPackageVersion -Name $env:BHProjectName -ErrorAction Stop
+        [version]$GithubVersion = Get-MetaData -Path $env:BHPSModuleManifest -PropertyName ModuleVersion -ErrorAction Stop
+        if ($GalleryVersion -ge $GithubVersion)
+        {
+            Update-Metadata -Path $env:BHPSModuleManifest -PropertyName ModuleVersion -Value $GalleryVersion -ErrorAction stop
+        }
+    }
+    Catch
+    {
+        "Failed to update version for '$env:BHProjectName': $_.`nContinuing with existing version"
+    }
+}
+
+Task Deploy -Depends Build {
+    $lines
+
+    # Gate deployment
+    if (#$true
+        $ENV:BHBuildSystem -ne 'Unknown' -and
+        $ENV:BHBranchName -eq "master" -and
+        $ENV:BHCommitMessage -match '!deploy'
+    )
+    {
+        $Params = @{
+            Path  = $ProjectRoot
+            Force = $true
+        }
+
+        Invoke-PSDeploy @Verbose @Params
+    }
+    else
+    {
+        "Skipping deployment: To deploy, ensure that...`n" +
+        "`t* You are in a known build system (Current: $ENV:BHBuildSystem)`n" +
+        "`t* You are committing to the master branch (Current: $ENV:BHBranchName) `n" +
+        "`t* Your commit message includes !deploy (Current: $ENV:BHCommitMessage)"
+    }
+}
+
