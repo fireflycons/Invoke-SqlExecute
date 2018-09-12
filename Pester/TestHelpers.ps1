91af4fb204a21f8bdf7cc600b421eb10282900b2
Alistair Mackay <34012094+fireflycons@users.noreply.github.com>
2018-09-12 15:25:36 +0100

    Fix filesystem access rights

--- Pester/TestHelpers.ps1
+++ Pester/TestHelpers.ps1
@@ -1,3 +1,17 @@
+function Grant-DirectoryAccess
+{
+    param
+    (
+        [string]$Directory
+    )
+
+    $everyoneSid = New-Object System.Security.Principal.SecurityIdentifier ([System.Security.Principal.WellKnownSidType]::WorldSid, $null)
+    $rule = New-Object  System.Security.AccessControl.FileSystemAccessRule($everyoneSid, "ReadAndExecute", "ObjectInherit, ContainerInherit", "NoPropagateInherit", "Allow")
+    $acl = Get-Acl -Path $Directory
+    $acl.AddAccessRule($rule) | Out-Null
+    Set-Acl -Path $Directory -AclObject $acl | Out-Null
+}
+
 function Get-AdventureWorksClone
 {
     Write-Host 'Cloning AdventureWorks from https://github.com/Microsoft/sql-server-samples'
@@ -6,18 +20,18 @@ function Get-AdventureWorksClone
         Push-Location $env:TEMP
         Write-Host "git clone -q -n https://github.com/Microsoft/sql-server-samples"
         git clone -q -n https://github.com/Microsoft/sql-server-samples 2>&1 | ForEach-Object { $_.ToString() } | Out-Host
-        Set-Location sql-server-samples 
+        Set-Location sql-server-samples
         Write-Host "git config core.sparsecheckout true"
         git config core.sparsecheckout true 2>&1 | ForEach-Object { $_.ToString() } | Out-Host
         Write-Host "git config core.autocrlf true"
         git config core.autocrlf true 2>&1 | ForEach-Object { $_.ToString() } | Out-Host
-        'samples/databases/adventure-works/*' | Out-File -Append -Encoding ascii .git/info/sparse-checkout 
+        'samples/databases/adventure-works/*' | Out-File -Append -Encoding ascii .git/info/sparse-checkout
         Write-Host "git checkout -q"
         git checkout -q 2>&1 | ForEach-Object { $_.ToString() } | Out-Host
-    
+
         $adventureWorksOltp = Join-Path $env:TEMP 'sql-server-samples\samples\databases\adventure-works\oltp-install-script'
         $adventureWorksDw = Join-Path $env:TEMP 'sql-server-samples\samples\databases\adventure-works\data-warehouse-install-script'
-    
+
         if (-not ((Test-Path -Path $adventureWorksOltp -PathType Container) -and (Test-Path $adventureWorksDw -PathType Container)))
         {
             Write-Warning 'AdventureWorks not cloned as expected'
@@ -25,18 +39,15 @@ function Get-AdventureWorksClone
         else
         {
             # Grant access to Everyone on the files so that SQL server service account can read
-            $everyoneSid = New-Object System.Security.Principal.SecurityIdentifier ([System.Security.Principal.WellKnownSidType]::WorldSid, $null)
-            $rule = New-Object  System.Security.AccessControl.FileSystemAccessRule($everyoneSid, "ReadAndExecute", "ObjectInherit, ContainerInherit", "NoPropagateInherit", "Allow")
-            $acl = Get-Acl -Path (Join-Path $env:TEMP 'sql-server-samples\samples\databases\adventure-works')
-            $acl.AddAccessRule($rule) | Out-Null
-            Set-Acl -Path (Join-Path $env:TEMP 'sql-server-samples\samples\databases\adventure-works') -AclObject $acl | Out-Null
+            Grant-DirectoryAccess -Directory $adventureWorksOltp
+            Grant-DirectoryAccess -Directory $adventureWorksDw
         }
     }
     catch
     {
         Write-Host -ForegroundColor Red "Error downloading AdventureWorks: $($_.Exception.Message)"
         $_.ScriptStackTrace
-        
+
     }
     finally
     {
@@ -45,7 +56,7 @@ function Get-AdventureWorksClone
 
     New-Object PSObject -Property @{
         OltpDir = $adventureWorksOltp
-        DwDir = $adventureWorksDw 
+        DwDir   = $adventureWorksDw
     }
 }
 
